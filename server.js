25c1be4246c0d17694ad927ef50e2f4174823d62
trabbster <david.avetisyan@trabbu.com>
2020-07-14 13:35:33 -0700

    added server cache

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ app.use(cors());
 
 //current apiKEY
 
-const apiKey = "O1XAGUnPQv0VxIiDzQTITaBbUAX1aH7n";
-// const apiKey = "ZSTWOA1o8MlVaxWo79PfdAqvYYeEU8gJ";
+// const apiKey = "O1XAGUnPQv0VxIiDzQTITaBbUAX1aH7n";
+const apiKey = "ZSTWOA1o8MlVaxWo79PfdAqvYYeEU8gJ";
 // const apiKey = 'JSAz93UV0ixfmyqkwA4vlbNx1FB3KCdA';
+
+let serverCache = {}
 
 //get function that returns current conditions when given an IP
 app.get(`/current_conditions_with_IP/:ip`, async (req, res) => {
@@ -22,22 +24,34 @@ app.get(`/current_conditions_with_IP/:ip`, async (req, res) => {
     var key = "this didnt work";
     const promise = new Promise((resolve, reject) => {
         console.log(`Fetching location key for IP Address: ${req.params.ip}`);
-        fetch(`http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=${apiKey}&q=${req.params.ip}&language=en-us&details=false`, requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                var obj = JSON.parse(result);
-                console.log(`Retrieved location key for IP Address: ${req.params.ip}`);
-                key = obj.Key;
-                console.log(`Fetchin current conditions for IP Address: ${req.params.ip}`);
-                fetch(`http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${apiKey}&language=en-us&details=false`, requestOptions)
-                    .then(response => response.text())
-                    .then(result => {
-                        var obj = JSON.parse(result);
-                        console.log(`Retrieved current conditions for IP Address: ${req.params.ip}`);
-                        console.log(obj);
-                        resolve(obj);
-                    });
-            });
+
+        serverCache['current_conditions_with_IP'] = serverCache['current_conditions_with_IP'] ? serverCache['current_conditions_with_IP'] : {}
+
+        const cacheExists = !!serverCache['current_conditions_with_IP'][req.params.ip];
+
+        if (cacheExists) {
+            return resolve(serverCache['current_conditions_with_IP'][req.params.ip])
+        } else {
+            fetch(`http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=${apiKey}&q=${req.params.ip}&language=en-us&details=false`, requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    var obj = JSON.parse(result);
+                    console.log(`Retrieved location key for IP Address: ${req.params.ip}`);
+                    key = obj.Key;
+                    console.log(`Fetchin current conditions for IP Address: ${req.params.ip}`);
+                    fetch(`http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${apiKey}&language=en-us&details=false`, requestOptions)
+                        .then(response => response.text())
+                        .then(result => {
+                            var obj = JSON.parse(result);
+                            serverCache['current_conditions_with_IP'][req.params.ip] = obj // store cache
+                            console.log(`Stored in cache serverCache['current_conditions_with_IP'][req.params.ip] : ${serverCache['current_conditions_with_IP'][req.params.ip]}`)
+                            console.log(`Retrieved current conditions for IP Address: ${req.params.ip}`);
+                            console.log(obj);
+                            resolve(obj);
+                        });
+                });
+        }
+
     });
     const myPromise = await promise;
     res.send(myPromise);
@@ -50,21 +64,32 @@ app.get(`/5_day_forecast_with_IP/:ip`, async (req, res) => {
     var key = "this didnt work";
     const promise = new Promise((resolve, reject) => {
         console.log(`Fetching location key for IP Address: ${req.params.ip}`);
-        fetch(`http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=${apiKey}&q=${req.params.ip}&language=en-us&details=false`, requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                var obj = JSON.parse(result);
-                console.log(`Retrieved location key for IP Address: ${req.params.ip}`);
-                key = obj.Key;
-                console.log(`Fetchin 5 day forecast for IP Address: ${req.params.ip}`);
-                fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=${apiKey}&language=en-us&details=false&metric=false`, requestOptions)
-                    .then(response => response.text())
-                    .then(result => {
-                        var obj = JSON.parse(result);
-                        console.log(`Retrieved 5 day forecast for IP Address: ${req.params.ip}`);
-                        resolve(obj);
-                    });
-            });
+
+        serverCache['5_day_forecast_with_IP'] = serverCache['5_day_forecast_with_IP'] || {}
+        const cacheExists = serverCache['5_day_forecast_with_IP'][req.params.ip]
+
+        if (cacheExists) {
+            resolve(serverCache['5_day_forecast_with_IP'][req.params.ip])
+        } else {
+            fetch(`http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=${apiKey}&q=${req.params.ip}&language=en-us&details=false`, requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    var obj = JSON.parse(result);
+                    console.log(`Retrieved location key for IP Address: ${req.params.ip}`);
+                    key = obj.Key;
+                    console.log(`Fetchin 5 day forecast for IP Address: ${req.params.ip}`);
+                    fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=${apiKey}&language=en-us&details=false&metric=false`, requestOptions)
+                        .then(response => response.text())
+                        .then(result => {
+                            var obj = JSON.parse(result);
+                            serverCache['5_day_forecast_with_IP'][req.params.ip] = obj
+                            console.log(`Stored in cache serverCache['5_day_forecast_with_IP'][req.params.ip] : ${serverCache['5_day_forecast_with_IP'][req.params.ip]}`)
+                            console.log(`Retrieved 5 day forecast for IP Address: ${req.params.ip}`);
+                            resolve(obj);
+                        });
+                });
+        }
+
     });
     const myPromise = await promise;
     res.send(myPromise);
