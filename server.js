78bac040eb9bf4b6b98f30fb4f396db2d692bea7
trabbster <david.avetisyan@trabbu.com>
2020-08-13 19:16:12 -0700

    adding server cache

--- server.js
+++ server.js
@@ -16,96 +16,140 @@ const apiKey = 'O1XAGUnPQv0VxIiDzQTITaBbUAX1aH7n';
 
 let serverCache = {};
 
+const useDB = false;
+
 //"type" paramater should be a string that is either "zip" or "ip"
-async function getLocationKey(param, type) {
-    let url = `http://dataservice.accuweather.com`;
-    if (type === 'zip') {
-        //url for zip code
-        url =
-            url +
-            `/locations/v1/postalcodes/search?apikey=${apiKey}&q=${param}&language=en-us&details=false`;
-    } else if (type === 'ip') {
-        //url for ip address
-        url =
-            url +
-            `/locations/v1/cities/ipaddress?apikey=${apiKey}&q=${param}&language=en-us&details=false`;
-    } else {
-        console.log('In getLocationKey()... incorrect type has been provided');
+async function getLocationKey(param, type = 'ip') {
+    if (useDB) {
+        const exists = await weather_database.location_key_exists(param);
+        if (exists) {
+            return await weather_database.get_location_key(param);
+        }
     }
-    const promise = new Promise((resolve, reject) => {
-        fetch(url)
-            .then((response) => response.text())
-            .then((result) => {
-                var obj = JSON.parse(result);
-                console.log(`Retrieved location key for (1): ${param}`);
-                key = obj.Key;
-                weather_database.store_location_key(key, param);
-                resolve(key);
-            });
-    });
-    const myPromise = await promise;
-    //res.send(myPromise);
-    return myPromise;
+    if (!serverCache[param]) {
+        let url = `http://dataservice.accuweather.com`;
+        if (type === 'zip') {
+            //url for zip code
+            url =
+                url +
+                `/locations/v1/postalcodes/search?apikey=${apiKey}&q=${param}&language=en-us&details=false`;
+        } else if (type === 'ip') {
+            //url for ip address
+            url =
+                url +
+                `/locations/v1/cities/ipaddress?apikey=${apiKey}&q=${param}&language=en-us&details=false`;
+        } else {
+            console.log(
+                'In getLocationKey()... incorrect type has been provided'
+            );
+        }
+        const promise = new Promise((resolve, reject) => {
+            fetch(url)
+                .then((response) => response.text())
+                .then((result) => {
+                    var obj = JSON.parse(result);
+                    // console.log(`Retrieved location key for (1): ${param}`);
+                    key = obj.Key;
+                    if (useDB) {
+                        weather_database.store_location_key(key, param);
+                    }
+                    resolve(key);
+                });
+        });
+        const myPromise = await promise;
+        serverCache[param] = myPromise;
+        console.log(
+            `Nothing in cache, fetched getLocationKey response: `,
+            myPromise
+        );
+        return myPromise;
+    } else {
+        console.log(`Returning from getLocationKey cache`);
+        return serverCache[param];
+    }
 }
 
 async function getCurrentConditions(locationKey) {
     console.log(
         `Fetching current conditions for location key (2): ${locationKey}`
     );
-    const promise = new Promise((resolve, reject) => {
-        fetch(
-            `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}&language=en-us&details=false`
-        )
-            .then((response) => response.text())
-            .then((result) => {
-                var obj = JSON.parse(result);
-                console.log(
-                    `Retrieved current conditions for location key (3): ${locationKey}`
-                );
-                console.log(obj);
-                resolve(obj);
-            });
-    });
-    const myPromise = await promise;
-    return myPromise;
+    if (!serverCache['getCurrentConditions']) {
+        const promise = new Promise((resolve, reject) => {
+            fetch(
+                `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}&language=en-us&details=false`
+            )
+                .then((response) => response.text())
+                .then((result) => {
+                    var obj = JSON.parse(result);
+                    console.log(
+                        `Retrieved current conditions for location key (3): ${locationKey}`
+                    );
+                    console.log(obj);
+                    resolve(obj);
+                });
+        });
+        const myPromise = await promise;
+        serverCache['getCurrentConditions'] = myPromise;
+        console.log(
+            `Nothing in cache, fetched getCurrentConditions response: `,
+            myPromise
+        );
+        return myPromise;
+    } else {
+        console.log(`Returning from getCurrentConditions cache`);
+        return serverCache['getCurrentConditions'];
+    }
 }
 
 async function getFiveDayForecast(locationKey) {
-    const promise = new Promise((resolve, reject) => {
-        fetch(
-            `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}&language=en-us&details=false&metric=false`
-        )
-            .then((response) => response.text())
-            .then((result) => {
-                var obj = JSON.parse(result);
-                console.log(
-                    `Retrieved 5 day forecast for location key(8): ${locationKey}`
-                );
+    if (!serverCache['getFiveDayForecast']) {
+        const promise = new Promise((resolve, reject) => {
+            fetch(
+                `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}&language=en-us&details=false&metric=false`
+            )
+                .then((response) => response.text())
+                .then((result) => {
+                    var obj = JSON.parse(result);
+                    console.log(
+                        `Retrieved 5 day forecast for location key(8): ${locationKey}`
+                    );
 
-                resolve(obj);
-            });
-    });
-    const myPromise = await promise;
-    return myPromise;
+                    resolve(obj);
+                });
+        });
+        const myPromise = await promise;
+        serverCache['getFiveDayForecast'] = myPromise;
+        console.log(
+            `Nothing in cache, fetched getFiveDayForecast response: `,
+            myPromise
+        );
+        return myPromise;
+    } else {
+        console.log(`Returning from getFiveDayForecast cache`);
+        return serverCache['getFiveDayForecast'];
+    }
 }
-
 //get function that returns current conditions when given an IP
 app.get(`/current_conditions_with_IP/:ip`, async (req, res) => {
-    const locationKey = (await weather_database.location_key_exists(
-        req.params.ip
-    ))
-        ? await weather_database.get_location_key(req.params.ip)
-        : await getLocationKey(req.params.ip, 'ip');
+    // const locationKey = (await weather_database.location_key_exists(
+    //     req.params.ip
+    // ))
+    //     ? await weather_database.get_location_key(req.params.ip)
+    //     : await getLocationKey(req.params.ip, 'ip');
 
-    res.send(await getCurrentConditions(locationKey));
+    const locationKey = await getLocationKey(req.params.ip);
+    const currentConditionsRes = await getCurrentConditions(locationKey);
+    res.send(currentConditionsRes);
 });
 
 app.get(`/5_day_forecast_with_IP/:ip`, async (req, res) => {
-    const locationKey = (await weather_database.location_key_exists(
-        req.params.ip
-    ))
-        ? await weather_database.get_location_key(req.params.ip)
-        : await getLocationKey(req.params.ip, 'ip');
+    // const locationKey = (await weather_database.location_key_exists(
+    //     req.params.ip
+    // ))
+    //     ? await weather_database.get_location_key(req.params.ip)
+    //     : await getLocationKey(req.params.ip, 'ip');
 
-    res.send(await getFiveDayForecast(locationKey));
+    const locationKey = await getLocationKey(req.params.ip);
+    const fiveDayForecastRes = await getFiveDayForecast(locationKey);
+    res.send(fiveDayForecastRes);
 });
